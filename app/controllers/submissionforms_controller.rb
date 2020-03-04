bc4f1bc668d517a646a21a00f83bbc1bb5ef3fc5
IsaacLaquerre <killers3d@gmail.com>
2020-03-04 11:55:15 -0500

    Updated pages

---
+++ app/controllers/submissionforms_controller.rb
@@ -0,0 +1,16 @@
+class SubmissionformsController < ApplicationController
+    def index
+        @title = "Price Estimation Form"
+        @paeg_title = "Rocket Elevators - Price Estimation Form"
+    end
+    def new
+        @title = "New Price Estimation Form"
+        @page_title = "Rocket Elevators - New Price Estimation Form"
+    end
+    def create
+        render plain: params[:post].inspect
+    end
+    def show
+
+    end
+end
---
+++ app/helpers/submissionforms_helper.rb
@@ -0,0 +1,2 @@
+module SubmissionformsHelper
+end
