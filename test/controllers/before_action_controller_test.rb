bab057acbb22e24cba515629d01a8f35ec8a63d4
maxparent <maxparent7@gmail.com>
2020-03-05 11:32:44 -0500

    clean

--- test/controllers/before_action_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class BeforeActionControllerTest < ActionDispatch::IntegrationTest
-  test "should get :authenticate_user!" do
-    get before_action_:authenticate_user!_url
-    assert_response :success
-  end
-
-end
