dd9e616472ab8ff48e0a5d1067fd6f0834b6e74e
Farid A <farid.anooshehpour@gmail.com>
2020-03-06 12:44:08 -0500

    more css&js issues fixed

--- Gemfile
+++ Gemfile
@@ -80,3 +80,6 @@ gem 'capistrano3-puma'
 # gem 'bcrypt_pbkdf' (>= 1.0, < 2.0)
 gem 'ed25519', '>= 1.2'
 gem 'bcrypt_pbkdf', '>= 1.0'
+
+# Jquery for JS and Bootstraps /FA
+gem 'jquery-rails'
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( scripts.js owl.carousel.min.js demo.revolution_slider.js jquery.themepunch.revolution.min.js jquery.themepunch.tools.min.js jquery-3.3.1.min.js)
+Rails.application.config.assets.precompile += %w( jquery-3.3.1.min.js scripts.js jquery.themepunch.tools.min.js jquery.themepunch.revolution.min.js demo.revolution_slider.js  bootstrap.min.js owl.carousel.min.js jquery.magnific-popup.min.js jquery.mixitup.min.js custom.fle_upload.js)
 Rails.application.config.assets.precompile += %w( bootstrap.min.css extralayers.css settings.css essentials.css layout.css header-1.css darkblue.css)
 
 Rails.application.config.assets.precompile = ["manifest.js"]
